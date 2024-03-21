7f49527bc920237809871822b2c716543b9e4d85
jahoroshi4y <jahoroshi4y@gmail.com>
2024-03-21 13:29:09 +0300

    oop hw stable

--- tms/parsing/requests_html/parser_hw.py
+++ tms/parsing/requests_html/parser_hw.py
@@ -8,27 +8,31 @@ import json
 
 class Parser(ABC):
     
-    def __init__(self, url, pages=2, headers=None):
-        self.url = url
-        self.pages = pages
+    def __init__(self, **kwargs):
+        self.pages = 2
+        self.headers = None
+        self.proxies = None
+        for key, value in kwargs.items():
+            setattr(self, key, value)
         self.session = HTMLSession(browser_args=["--no-sandbox", "--user-agent='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 YaBrowser/20.9.3.136 Yowser/2.5 Safari/537.36'", "--ignore-certificate-errors", "--headless"])
-        self.headers = headers
+        self.report = []
         self.file_path = os.path.dirname(__file__) + "/data/"
+        self.site_name = f'{self.url.split("://")[1].split("/")[0].replace(".", "_")}'
+
         
     
     def get_data(self, num=1):
         url = self.url.format(num)
         self.response = self.session.get(url, headers=self.headers)
         if not self.response.status_code == 200:
-            self.response = False
+            raise ValueError("GET", self.response.status_code)
        
     def render_js(self, scrolldown=False):
         self.response.html.render(wait=random.uniform(2, 4), scrolldown=scrolldown)
      
         
     def save_response(self, num=""):
-        self.name = f'{self.url.split("://")[1].split("/")[0].replace(".", "_")}_{num}'
-        with open(f'{self.file_path}{self.name}.html', "w") as file:
+        with open(f'{self.file_path}{self.site_name}_{num}.html', "w") as file:
             file.write(self.response.html.html)
             
             
@@ -80,18 +84,29 @@ class ProxyParserHTML(Parser):
                     if pr_type in i.text.lower():
                         self.proxy_types[pr_type].append(f'{ip}:{port}')
                         
-        for file_name, proxies in self.proxy_types.items():
-            self.save_csv(file_name, proxies)
+        self.report = []                   
+        for file_name, prox in self.proxy_types.items():
+            self.save_csv(file_name, prox)
+            self.report.append(f'Получено {file_name} - {len(prox)}')
 
     def __call__(self):
-        for i in range(1, self.pages):
-            self.get_data(i)
-            if self.is_ip(self.response.html.text):
-                self.parce()
-                print(i)     
-            else:
-                print("finish")
-                break
+        try:
+            for i in range(1, int(self.pages)):
+                self.get_data()
+                if self.is_ip(self.response.html.text):
+                    self.parce()
+                    print(f'Страница {i} объкта {self.site_name.upper()} обработана успешно.')
+                    print(*self.report)
+                else:
+                    if self.report:
+                        print(f'Обработка объекта {self.site_name.upper()} завершена успешно')
+                        print(*self.report)
+                    else:
+                        print(f'{self.name} --- данные не получены')                      
+                    break
+                
+        except Exception as e:
+            print("__call__", e)
             
 
 
@@ -117,13 +132,26 @@ class ProxyParserAPI(ProxyParserHTML):
             if el['socks5'] == '1':
                 self.proxy_types['socks5'].append(f"{el['ip']}:{el['port']}")
                 
-        for file_name, proxies in self.proxy_types.items():
-            self.save_csv(file_name, proxies)                                
+        for file_name, prox in self.proxy_types.items():
+            self.save_csv(file_name, prox)                                
         
                 
 
-ps1 = ProxyParserHTML("https://hidemy.io/en/proxy-list/")
-ps1()                    
+# ps1 = ProxyParserHTML("https://hidemy.io/en/proxy-list/")
+# ps1()                    
             
 # ps2 = ProxyParserAPI("https://fineproxy.org/wp-content/themes/fineproxyorg/proxy-list.php?0.20522686081524832", headers={'Referer': 'https://fineproxy.org/'})
 # ps2()
+
+# tasks = [{'url': "https://advanced.name/ru/freeproxy?page={}", 'pages': '10'}]
+
+# for url in tasks:
+#     obj = ProxyParserHTML(**url)
+#     obj()
+    
+    
+tasks = [{'url': "https://fineproxy.org/wp-content/themes/fineproxyorg/proxy-list.php?0.20522686081524832", 'headers': {'Referer': 'https://fineproxy.org/'}}]
+
+for url in tasks:
+    obj = ProxyParserAPI(**url)
+    obj()
